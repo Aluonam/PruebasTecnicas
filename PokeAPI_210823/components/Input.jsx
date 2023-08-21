f06c93ed5cefd4ba6a563f2e5e3d503b865e5fc6
Paula Cano <paulacanoroman@gmail.com>
2023-08-21 09:59:38 +0200

    Condición aparece imagen en img
    {datosAPI!=='' && <img src={datosAPI} alt='imagen pokemon'></img>} 
    Si datosAPI (donde se almacena la url) es distinto a vacio ('') saca imagen con los datosAPI (uso de &&)

--- PokeAPI_210823/components/Input.jsx
+++ PokeAPI_210823/components/Input.jsx
@@ -3,16 +3,16 @@ import React, {useState} from 'react'
 const Input = () => {
 
     //---------------useState datos de la API---------------------
-    const [datosAPI, setDatosAPI] = useState("no hay datos en la API")
+    const [datosAPI, setDatosAPI] = useState("")
     //---------------useState datos que introduce el usuario------
-    const [textoUsuario, setTextoUsuario] = useState("no se ha escrito nada")
+    const [textoUsuario, setTextoUsuario] = useState('')
 
     const llamadaAPI = async (textoUsuario) => {
         try{
         const url = `https://pokeapi.co/api/v2/pokemon/${textoUsuario}`;
         const llamada = await fetch(url);
         const datos = await llamada.json();
-       setDatosAPI(datos.sprites.front_default) //CRISSSSSSSSSSSSSSSIS
+       setDatosAPI(datos.sprites.front_default) 
     } catch(error){
         console.log("Error detectado",error)
     }
@@ -21,7 +21,7 @@ const Input = () => {
 
   return (
     <>
-    {datosAPI}
+    {datosAPI!=='' && <img src={datosAPI} alt='imagen pokemon'></img>}
     <input onChange={(e)=>{setTextoUsuario(e.target.value)}}></input>
     <button onClick={()=>{llamadaAPI(textoUsuario)}}>Clic</button>
     </>
