c0a867f4d340718e89dc7e6ec5376b0a9047e236
Aluon <paulacanoroman@gmail.com>
2023-08-29 13:27:27 +0200

    feat/ POST API enviar info de input a la api
    Al enviar el formulario, utiliza el efecto useEffect para realizar una solicitud POST a la URL https://jsonplaceholder.typicode.com/todos con el texto de la nueva tarea.
    Actualiza el estado para incluir la nueva tarea en la lista.

--- prueba-axios/component/TodoList.jsx
+++ prueba-axios/component/TodoList.jsx
@@ -5,18 +5,31 @@ const TodoList = () => {
 
 
     const [objList, setObjList] = useState([ {id: 1, text: 'doing something', completed: true }])
-
+    const [inputValue, setInputValue] = useState()
 
     useEffect(() => {
       llamadaApi()
     }, [])
     
+    //--------------------- GET API
     const llamadaApi = async ()=>{
         const llamada =  await axios.get('https://jsonplaceholder.typicode.com/todos')
         console.log("ejemplo",llamada);
         setObjList(llamada.data);   
     }
-    
+    //-------------------- POST API
+    const createTask = () => {
+        const baseURL= `https://jsonplaceholder.typicode.com/todos`
+        const bodyPOST = {
+            title: inputValue
+          }
+
+        axios
+          .post(baseURL,bodyPOST )
+          .then((response) => {
+            console.log("respuesta",response.data);
+          });
+      }
       const listaDatos = objList.map((valorActual)=>{
         return (
             <ul key={valorActual.id}>
@@ -31,12 +44,19 @@ const TodoList = () => {
       }
       
 
+   
+
 
   return (
     <>
     <h3>ToDoList</h3>
     <button onClick={()=>handleClickTareasCompletas()}>Eliminar Tareas Completadas</button>
     {listaDatos}
+    <br></br>
+    <div>
+    <input onChange={(e)=>{setInputValue(e.target.value)}}></input>
+    <button onClick={()=>{createTask()}}>Añadir nueva tarea</button>
+    </div>
     </>
   )
 }
